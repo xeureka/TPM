3fcb8b67bfb0e008d005e124d5ce7e13f3e9d2da
xeureka <156827630+xeureka@users.noreply.github.com>
2026-06-03 22:29:01 +0300

    inital file structure used directly from the original project on build ur own x

---
+++ src/cli.ts
@@ -0,0 +1,39 @@
+import yargs from "yargs";
+// import pm from "."
+
+// The file is for CLI usage
+
+yargs
+  .usage("tpm <command> [args]")
+  .version()
+  .alias("v", "version")
+  .help()
+  .alias("h", "help")
+  .command(
+    "install",
+    "Install the dependencies.",
+    (argv) => {
+      argv.option("production", {
+        type: "boolean",
+        description: "Install production dependencies only.",
+      });
+
+      argv.boolean("save-dev");
+      argv.boolean("dev");
+      argv.alias("D", "dev");
+
+      return argv;
+    },
+    //     pm
+  )
+  .command(
+    "*",
+    "Install the dependencies",
+    (argv) =>
+      argv.option("production", {
+        type: "boolean",
+        description: "Install production dependencies only",
+      }),
+    //   pm
+  )
+  .parse();
